d31fd0e225df0dea0bf4bb31cc312a2474956d27
NikitaSec <santianogames@gmial.com>
2024-11-24 14:18:20 +0300

    First commit

---
+++ Fruit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ThreeInLine
+{
+    public class Fruit
+    {
+        public Vector2 Position { get; set; }
+        public Image Body { get; set; }
+    }
+}
